2f05087f608c7f89dce6f8350a2cbd6d34d23595
sanjay-kv2769 <93617720+sanjay-kv2769@users.noreply.github.com>
2022-08-09 22:17:27 +0530

    CRUD Operations
    Hotels crud operation and feedback integration

---
+++ src/app/ernakulam/ernakulam.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ernakulam',
+  templateUrl: './ernakulam.component.html',
+  styleUrls: ['./ernakulam.component.css']
+})
+export class ErnakulamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/guide-reg/guide-reg.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guide-reg',
+  templateUrl: './guide-reg.component.html',
+  styleUrls: ['./guide-reg.component.css']
+})
+export class GuideRegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  signup_img = "./assets/img/illustrations/tour-guide-pana.png"
+
+}
